7cd12e9723e49007ce2a43aa5d802e53cb90e13e
Anisha Gupta <anisha.gupta@cloverbaytechnologies.com>
2021-08-31 17:38:07 +0530

    Some css is fixed

--- src/CreditCard.js
+++ src/CreditCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Cards from "react-credit-cards";
 import "react-credit-cards/es/styles-compiled.css";
@@ -11,11 +11,11 @@ const CreditCard = () => {
   let [expiry, SetExpiry] = useState("");
   const [cvc, SetCvc] = useState("");
   const [focus, SetFocus] = useState("");
-  const handleDate = (e) => {
+  const handleDate = e => {
     SetMonth(e.target.value);
     SetExpiry(e.target.value);
   };
-  const handleExpiry = (e) => {
+  const handleExpiry = e => {
     SetExpiry(month.concat(e.target.value));
   };
 
@@ -32,120 +32,124 @@ const CreditCard = () => {
       </div>
 
       <br />
-      <form>
-        <div className="row">
-          <div className="col-sm-11">
-            <label for="name">Card Number</label>
-            <input
-              type="tel"
-              className="form-control"
-              value={number}
-              name="number"
-              maxlength="16"
-              pattern="[0-9]+"
-              onChange={(e) => {
-                SetNumber(e.target.value);
-              }}
-              onFocus={(e) => SetFocus(e.target.name)}
-            ></input>
-          </div>
-        </div>
-        <br />
-        <div className="row">
-          <div className="col-sm-11">
-            <label for="name">Card Name</label>
-            <input
-              type="text"
-              className="form-control"
-              value={name}
-              name="name"
-              onChange={(e) => {
-                SetName(e.target.value);
-              }}
-              onFocus={(e) => SetFocus(e.target.name)}
-            ></input>
-          </div>
-        </div>
-        <br />
-        <div className="row">
-          <div
-            className="col=sm-8"
-            style={{
-              ...{ "padding-right": "12em" },
-              ...{ "padding-left": "1em" }
-            }}
-          >
-            <label for="month">Expiration Date</label>
-          </div>
-          <div className="col=sm-4">
-            <label for="cvv">CVV</label>
-          </div>
-        </div>
 
-        <div className="row">
-          <div className="col-sm-4">
-            <select
-              className="form-control"
-              name="expiry"
-              onChange={handleDate}
-            >
-              <option value=" ">Month</option>
-              <option value="01">Jan</option>
-              <option value="02">Feb</option>
-              <option value="03">Mar</option>
-              <option value="04">April</option>
-              <option value="05">May</option>
-              <option value="06">June</option>
-              <option value="07">July</option>
-              <option value="08">Aug</option>
-              <option value="09">Sep</option>
-              <option value="10">Oct</option>
-              <option value="11">Nov</option>
-              <option value="12">Dec</option>
-            </select>
+      <div className="main-div" style={{marginLeft: "430px"}}>
+        <form>
+          <div className="row">
+            <div className="col-sm-11">
+              <label for="name">Card Number</label>
+              <input
+                type="tel"
+                className="form-control"
+                value={number}
+                name="number"
+                maxlength="16"
+                pattern="[0-9]+"
+                onChange={e => {
+                  SetNumber(e.target.value);
+                }}
+                onFocus={e => SetFocus(e.target.name)}
+              ></input>
+            </div>
           </div>
-          &nbsp;
-          <div className="col-sm-4">
-            <select
-              className="form-control"
-              name="expiry"
-              onChange={handleExpiry}
-            >
-              <option value=" ">Year</option>
-              <option value="21">2021</option>
-              <option value="22">2022</option>
-              <option value="23">2023</option>
-              <option value="24">2024</option>
-              <option value="25">2025</option>
-              <option value="26">2026</option>
-              <option value="27">2027</option>
-              <option value="28">2028</option>
-              <option value="29">2029</option>
-              <option value="30">2030</option>
-            </select>
+          <br />
+          <div className="row">
+            <div className="col-sm-11">
+              <label for="name">Card Name</label>
+              <input
+                type="text"
+                className="form-control"
+                value={name}
+                name="name"
+                onChange={e => {
+                  SetName(e.target.value);
+                }}
+                onFocus={e => SetFocus(e.target.name)}
+              ></input>
+            </div>
           </div>
-          <div className="col-sm-3">
-            <input
-              type="tel"
-              name="cvc"
-              maxlength="3"
-              className=" form-control card"
-              value={cvc}
-              pattern="\d*"
-              onChange={(e) => {
-                SetCvc(e.target.value);
+          <br />
+          <div className="row">
+            <div
+              className="col=sm-8"
+              style={{
+                ...{"padding-right": "12em"},
+                ...{"padding-left": "1em"},
               }}
-              onFocus={(e) => SetFocus(e.target.name)}
-            ></input>
+            >
+              <label for="month">Valid Through</label>
+            </div>
+            <div className="col=sm-4">
+              <label for="cvv">CVV</label>
+            </div>
           </div>
-        </div>
-        <br />
-        <input
-          type="submit"
-          className="btn btn-secondary form-control"
-          value="Submit"
-        />
-      </form>
+
+          <div className="row">
+            <div className="col-sm-4">
+              <select
+                className="form-control"
+                name="expiry"
+                onChange={handleDate}
+              >
+                <option value=" ">Month</option>
+                <option value="01">Jan</option>
+                <option value="02">Feb</option>
+                <option value="03">Mar</option>
+                <option value="04">April</option>
+                <option value="05">May</option>
+                <option value="06">June</option>
+                <option value="07">July</option>
+                <option value="08">Aug</option>
+                <option value="09">Sep</option>
+                <option value="10">Oct</option>
+                <option value="11">Nov</option>
+                <option value="12">Dec</option>
+              </select>
+            </div>
+            &nbsp;
+            <div className="col-sm-4">
+              <select
+                className="form-control"
+                name="expiry"
+                onChange={handleExpiry}
+              >
+                <option value=" ">Year</option>
+                <option value="21">2021</option>
+                <option value="22">2022</option>
+                <option value="23">2023</option>
+                <option value="24">2024</option>
+                <option value="25">2025</option>
+                <option value="26">2026</option>
+                <option value="27">2027</option>
+                <option value="28">2028</option>
+                <option value="29">2029</option>
+                <option value="30">2030</option>
+              </select>
+            </div>
+            <div className="col-sm-3">
+              <input
+                type="tel"
+                name="cvc"
+                maxlength="3"
+                className=" form-control card"
+                value={cvc}
+                pattern="\d*"
+                onChange={e => {
+                  SetCvc(e.target.value);
+                }}
+                onFocus={e => SetFocus(e.target.name)}
+              ></input>
+            </div>
+          </div>
+          <br />
+          <input
+            style={{backgroundColor: "#1D5AF3", color: "#fff"}}
+            type="submit"
+            className="btn btn-secondary form-control"
+            value="Submit"
+          />
+        </form>
+      </div>
     </>
   );
 };
